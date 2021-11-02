4c88c2e9fa81db13b891203f98967da20fcc1081
Ross Shen <rossshen@qq.com>
2021-11-01 18:18:20 -0700

    added functional ballista

---
+++ Assets/Scripts/Ballista.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballista : MonoBehaviour
+{
+	public GameObject arrow;
+
+	private Vector3 offset; 	// start position of arrow relative to ballista
+	private bool cooldown; 		// Has it shoot for this animation?	
+
+	// Call when start
+	void Start()
+	{
+		// calculate offset of the arrow
+		offset = this.GetComponent<Renderer>().bounds.size;
+		offset.x /= 2;
+		offset.y = 0;
+		offset.z = -1;
+
+		cooldown = false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		string spriteName = this.GetComponent<SpriteRenderer>().sprite.name;
+
+		if( !cooldown && spriteName == "ballista_3" )
+		{
+			Shoot();
+			cooldown = true;
+		}
+		else if( cooldown && spriteName != "ballista_3" )
+		{
+			cooldown = false;
+		}
+    }
+
+	// shoot arrows automatically 
+	void Shoot()
+	{
+        Instantiate( arrow, this.transform.position - offset, this.transform.rotation );
+	}
+}
---
+++ Assets/Scripts/BallistaArrow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallistaArrow : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = this.transform.right * speed * -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+    private void OnTriggerEnter2D( Collider2D collision )
+	{
+        Destroy( gameObject ); 
+	}
+}
