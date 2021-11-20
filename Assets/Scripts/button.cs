c5b7406fda5e7a053ce0efe0e117c602b72f7493
Andrew <laz1105959326@gmail.com>
2021-11-19 18:09:27 -0800

    roof design

--- Assets/Scripts/button.cs
+++ Assets/Scripts/button.cs
@@ -5,40 +5,57 @@ using UnityEngine;
 public class button : MonoBehaviour
 {
     // Start is called before the first frame update
-    private bool exit;
+    private bool exit, colliding, oni, kumo;
     public bool pressed;
+    public int pos;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (exit == true && this.transform.position.y <= -1)
-        {
-            this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 0.1f);
-            pressed = false;
-        }
 
-        
+        if (this.transform.position.y <= pos && exit == true) this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 0.0005f);
+
+        if (this.transform.position.y >= pos) pressed = false;
+
+            if (oni == false && kumo == false)
+        {
+            exit = true;
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Oni" || collision.gameObject.tag == "Kumo")
+        if (collision.gameObject.tag == "Oni")
         {
+            oni = true;
             pressed = true;
             exit = false;
-            if (this.transform.position.y >= -1.5) this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 0.1f);
+            if (this.transform.position.y >= pos - 0.5) this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 0.1f);
+        }
+
+        if (collision.gameObject.tag == "Kumo")
+        {
+            kumo = true;
+            pressed = true;
+            exit = false;
+            if (this.transform.position.y >= pos - 0.5) this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 0.1f);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Oni" || collision.gameObject.tag == "Kumo")
+        if (collision.gameObject.tag == "Oni")
         {
-            exit = true;
+            oni = false;
+        }
+
+        if (collision.gameObject.tag == "Kumo")
+        {
+            kumo = false;
         }
     }
 }
