9a291ea548e90d2368d1a8e827b2d0f1669f6688
Ross Shen <rossshen@qq.com>
2021-11-21 00:11:30 -0800

    input key class to define global keyboard input

---
+++ Assets/Scripts/InputKey.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputKey
+{
+	// Kumo keyboard controls
+	public const KeyCode KumoLeft = KeyCode.A;
+	public const KeyCode KumoRight = KeyCode.D;
+	public const KeyCode KumoJump = KeyCode.G;
+	public const KeyCode KumoAttack = KeyCode.F;
+	public const KeyCode KumoInteract = KeyCode.W;
+
+	// Oni keyboard controls
+	public const KeyCode OniLeft = KeyCode.LeftArrow;
+	public const KeyCode OniRight = KeyCode.RightArrow;
+	public const KeyCode OniJump = KeyCode.Period;
+	public const KeyCode OniAttack = KeyCode.Comma;
+	public const KeyCode OniInteract = KeyCode.UpArrow;
+}
