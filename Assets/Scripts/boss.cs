e0ffbdce010b2175b24c66adb55d38cc3951a2c1
Andrew <laz1105959326@gmail.com>
2021-11-22 15:11:45 -0800

    parry blade

--- Assets/Scripts/boss.cs
+++ Assets/Scripts/boss.cs
@@ -7,7 +7,9 @@ public class boss : MonoBehaviour
     // Start is called before the first frame update
     private int summonTimer = 600;
     private int hp = 10;
-    public GameObject summon;
+    public int currentHP;
+    private int randomN;
+    public GameObject summon, blade;
     void Start()
     {
         
@@ -17,17 +19,28 @@ public class boss : MonoBehaviour
     void Update()
     {
         summonTimer--;
+        //stage1
+        if (currentHP > 7)
+        {
 
+        
         if (summonTimer == 0)
         {
             this.GetComponent<Animator>().SetBool("isSummoning", true);
+                //randomN = Random.Range(0, 2);
+                randomN = 1;
         }
         
         if (this.GetComponent<SpriteRenderer>().sprite.name == "boss_summon6")
         {
-            if (this.GetComponent<Animator>().GetBool("isSummoning"))Instantiate(summon, new Vector2(Random.Range(-10, 20), -2.7f), this.transform.rotation);
+            if (this.GetComponent<Animator>().GetBool("isSummoning"))
+            {
+                if (randomN == 0)Instantiate(summon, new Vector2(Random.Range(-10, 20), -2.7f), this.transform.rotation);
+                else if (randomN == 1) Instantiate(blade, this.transform.position, this.transform.rotation);
+            }
             summonTimer = 600;
             this.GetComponent<Animator>().SetBool("isSummoning", false);
+        }
         }
     }
 }
