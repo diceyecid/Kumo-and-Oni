a9a5108ff15a5efe3c70557c93e4fb6cde546ead
Andrew <laz1105959326@gmail.com>
2021-12-03 15:22:40 -0800

    redesign lvl3

--- Assets/Scripts/boss.cs
+++ Assets/Scripts/boss.cs
@@ -6,7 +6,7 @@ public class boss : MonoBehaviour
 {
     // Start is called before the first frame update
     private int summonTimer = 600;
-    private int hp = 10;
+    private int hp = 3;
     public int currentHP;
     private int randomN, stage = 0, num;
     public GameObject summon, blade, platform, ground;
