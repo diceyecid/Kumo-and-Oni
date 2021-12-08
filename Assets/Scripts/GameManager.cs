9eeecd25bf4ed5254891b07577fef77440de060e
Ross Shen <rossshen@qq.com>
2021-12-08 10:37:49 -0800

    basic game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+	private static GameManager _instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			return _instance;
+		}
+	}
+
+	// Awake is called when the script instance is being loaded
+	private void Awake()
+	{
+		_instance = this;
+		DontDestroyOnLoad( gameObject );
+	}
+	
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
-
-	void init()
-	{
-		SceneLoader.LoadLevel1();
-	}
 }
